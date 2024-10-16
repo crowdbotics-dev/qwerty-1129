226f7c89a9df100d7299f3c8571288ce5904e6ed
Crowdbotics <engineering@crowdbotics.com>
2024-10-16 09:37:43 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "qwerty-1129",
+  url: "https://qwerty-1129.botics.co",
+  api: "https://qwerty-1129.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
